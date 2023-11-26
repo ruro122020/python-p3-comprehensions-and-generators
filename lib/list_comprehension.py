1c3537f6e1ae0e4bd7e9176ce88f55f75310828d
Ruth <ruro122020@gmail.com>
2023-11-26 12:09:29 -0500

    returns_evens function complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+   # print([n / 2 for n in num_list])
+   # print([(n **2) - 1 for n in range(1, 11)])
+  even_num = list()
+  print(num_list)
+  for n in num_list:
+    print('n', n)
+    if(n % 2 == 0):
+      even_num.append(n)
+  return even_num
+
 
 def make_exclamation(sentence_list):
     pass
